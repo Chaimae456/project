cd82ea66a40e27471aaa3fb8c56a9828504b2969
Chaimae456 <solicode.chaimae@gmail.com>
2022-06-07 08:28:11 +0100

    done

--- projet/app/src/main/java/com/example/carpartsalpha/activities/LoginActivity.kt
+++ projet/app/src/main/java/com/example/carpartsalpha/activities/LoginActivity.kt
@@ -1,16 +1,22 @@
 package com.example.carpartsalpha.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.carpartsalpha.R
+import com.example.carpartsalpha.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         @Suppress("DEPRECATION")
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -21,6 +27,14 @@ class LoginActivity : AppCompatActivity() {
             )
         }
 
+        binding.tvRegister.setOnClickListener {
+            // Launch the register screen when the user clicks on the text.
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
+
+
 }
--- projet/app/src/main/java/com/example/carpartsalpha/activities/RegisterActivity.kt
+++ projet/app/src/main/java/com/example/carpartsalpha/activities/RegisterActivity.kt
@@ -1,16 +1,21 @@
 package com.example.carpartsalpha.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.carpartsalpha.R
+import com.example.carpartsalpha.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+            val view = binding.root
+        setContentView(view)
         @Suppress("DEPRECATION")
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -20,5 +25,27 @@ class RegisterActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarRegisterActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        binding.toolbarRegisterActivity.setNavigationOnClickListener{ onBackPressed() }
+    }
+    // END
 }
