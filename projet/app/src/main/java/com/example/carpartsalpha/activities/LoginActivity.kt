08eda298664a561164eeb04a9f8274acaf04128e
Chaimae456 <solicode.chaimae@gmail.com>
2022-06-08 02:06:02 +0100

    done

--- projet/app/src/main/java/com/example/carpartsalpha/activities/LoginActivity.kt
+++ projet/app/src/main/java/com/example/carpartsalpha/activities/LoginActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.carpartsalpha.R
 import com.example.carpartsalpha.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,15 +29,81 @@ class LoginActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+        binding.tvRegister.setOnClickListener(this)
+        binding.tvForgotPassword.setOnClickListener(this)
+        binding.btnLogin.setOnClickListener(this)
 
-        binding.tvRegister.setOnClickListener {
-            // Launch the register screen when the user clicks on the text.
-            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
 
     }
+    // In Login screen the clickable components are Login Button, ForgotPassword text and Register Text.
+    override fun onClick(view: View?) {
+        if (view != null) {
+            when (view.id) {
 
+                R.id.tv_forgot_password -> {
+                    val intent = Intent(this@LoginActivity, ForgotPasswordActivity ::class.java)
+                    startActivity(intent)
+                }
 
+                R.id.btn_login -> {
+                    logInRegisteredUser()
+                }
+
+                R.id.tv_register -> {
+                    // Launch the register screen when the user clicks on the text.
+                    val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+    // END
+    /**
+     * A function to validate the login entries of a user.
+     */
+    private fun validateLoginDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+            TextUtils.isEmpty(binding.etPassword    .text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            else -> {
+              //  showErrorSnackBar("Your details are valid.", false)
+                true
+            }
+        }
+    }
+    // END
+    /**
+     * A function to hundle the sing in of a user
+     */
+
+    private fun logInRegisteredUser(){
+        if(validateLoginDetails()){
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+            val password: String = binding.etPassword.text.toString().trim { it <= ' ' }
+
+        // login user firebase auth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+
+                    if(task.isSuccessful){
+                        // TODO send user to MAin activity
+                        showErrorSnackBar("You are logged in successfully.",false)
+                    }else{
+                        showErrorSnackBar(task.exception!!.message.toString(),true)
+                    }
+
+                }
+
+        }
+    }
 }
--- projet/app/src/main/java/com/example/carpartsalpha/activities/RegisterActivity.kt
+++ projet/app/src/main/java/com/example/carpartsalpha/activities/RegisterActivity.kt
@@ -1,15 +1,22 @@
 package com.example.carpartsalpha.activities
 
-import android.content.Intent
+
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.carpartsalpha.R
 import com.example.carpartsalpha.databinding.ActivityRegisterBinding
+import com.example.carpartsalpha.firestore.FirestoreClass
+import com.example.carpartsalpha.models.User
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +32,15 @@ class RegisterActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+        setupActionBar()
+
         binding.tvLogin.setOnClickListener {
-            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+           onBackPressed()
+        }
+
+        binding.btnRegister.setOnClickListener {
+            registerUser()
         }
     }
 
@@ -46,6 +58,127 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.toolbarRegisterActivity.setNavigationOnClickListener{ onBackPressed() }
+    }
+    // END
+
+    /**
+     * A function to validate the entries of a new user.
+     */
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(binding.etFirstName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etLastName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etConfirmPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            binding.etPassword.text.toString().trim { it <= ' ' } != binding.etConfirmPassword.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+            !binding.cbTermsAndCondition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> {
+                //showErrorSnackBar(resources.getString(R.string.registery_successfull), false)
+                true
+            }
+        }
+    }
+    // END
+
+
+    /**
+     * A function to register the user with email and password using FirebaseAuth.
+     */
+    private fun registerUser() {
+
+        // Check with validate function if the entries are valid or not.
+        if (validateRegisterDetails()) {
+
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+            val password: String = binding.etPassword.text.toString().trim { it <= ' ' }
+
+            // Create an instance and create a register a user with email and password.
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+                        // If the registration is successfully done
+                        if (task.isSuccessful) {
+
+                            // Firebase registered user
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+
+
+                            val user = User(
+                                firebaseUser.uid,
+                                binding.etFirstName.text.toString().trim { it <= ' ' },
+                                binding.etLastName.text.toString().trim { it <= ' ' },
+                                binding.etEmail.text.toString().trim { it <= ' ' }
+                           )
+
+
+                            FirebaseAuth.getInstance().signOut()
+                            // Finish the Register Screen
+                            finish()
+
+                            FirestoreClass().registerUser(this@RegisterActivity, user)
+                        } else {
+                            // If the registering is not successful then show error message.
+                            hideProgressDialog()
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    })
+        }
+    }
+    // END
+
+    /**
+     * A function to notify the success result of Firestore entry when the user is registered successfully.
+     */
+    fun userRegistrationSuccess() {
+
+        // Hide the progress dialog
+        hideProgressDialog()
+
+        // TODO Step 5: Replace the success message to the Toast instead of Snackbar.
+        Toast.makeText(
+            this@RegisterActivity,
+            resources.getString(R.string.register_success),
+            Toast.LENGTH_SHORT
+        ).show()
+
+
+        /**
+         * Here the new user registered is automatically signed-in so we just sign-out the user from firebase
+         * and send him to Intro Screen for Sign-In
+         */
+        FirebaseAuth.getInstance().signOut()
+        // Finish the Register Screen
+        finish()
     }
     // END
 }
